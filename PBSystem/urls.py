44288c6d84ec9cafab5cfac8a21e227c11cec990
bichanna <nobu.bichanna@gmail.com>
2021-02-25 01:37:41 +0000

    added updating and deleting function.

--- PBSystem/urls.py
+++ PBSystem/urls.py
@@ -14,6 +14,4 @@ app_name = "PBSystem"  # application name
 urlpatterns = [
 	path("", views.bank_account_data_list, name="bank_account_data_list"),
 	path("admin_user_list/<int>",views.admin_user_list_list, name="admin_user_list_list"),
-	path("<int:pk>/delete/",views.PBSystemDeleteView.as_view(), name="delete"),
-	path("<int:pk>/edit/",views.PBSystemUpdateView.as_view(), name="edit")
 ]
