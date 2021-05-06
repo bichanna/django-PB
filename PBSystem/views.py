4bafeb8eb1ca6920372e10c3a748bcf39e1fb133
bichanna <nobu.bichanna@gmail.com>
2021-05-06 01:39:29 +0000

    deleted a chunck of comment lines in views.py

--- PBSystem/views.py
+++ PBSystem/views.py
@@ -165,14 +165,6 @@ class CreateBankAccountView(LoginRequiredMixin, CreateView):
 	form_class = NewAccount
 
 	def form_valid(self, form):
-		# print("⭐"*10)
-		# customerName = CustomerList.objects.get(id=self.request.GET.get("i"))
-		# newBankAccount = "account"+str(BankAccounts.objects.filter(customer_name=customerName).count()+1)
-		
-		# newBankAccountSave = BankAccounts(bank_account_number=newBankAccount, customer_name=customerName)
-		# print(customerName, type(customerName))
-		# print(newBankAccount, type(newBankAccount))
-		# newBankAccountSave.save()
 		form.instance.customer_name = CustomerList.objects.get(id=self.request.GET.get("i"))
 		form.instance.bank_account_number = "account"+str(BankAccounts.objects.filter(customer_name=form.instance.customer_name).count()+1)
 		return super().form_valid(form)
